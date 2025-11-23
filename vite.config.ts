4561f39ca4b2ef03494a3b22293f6e9a2c834ed4
ppbenitez <ppbenitez332@gmail.com>
2025-11-22 21:51:38 -0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,18 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   root: './client',
+
+  build: {
+    outDir: '../dist',     // <--- dist volta pra raiz
+    emptyOutDir: true,     // limpa a pasta dist antes
+  },
+
   server: {
     host: true,
     allowedHosts: true,
     port: 5173,
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './client/src'),
